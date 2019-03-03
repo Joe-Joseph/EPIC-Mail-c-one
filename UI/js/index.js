60d58dda92b3e841a62a3d7b49395c061489a7f9
Joseph <jostheblessing@gmail.com>
2019-03-03 23:40:26 +0200

    Add some icons to use in the pages

--- UI/js/index.js
+++ UI/js/index.js
@@ -5,60 +5,60 @@ window.onload = function() {
     var submit = document.getElementById("submitButton");
     var submitButtonAdmin= document.getElementById("submitButtonAdmin");
         signIn.onmouseover = function(){
-            this.style.backgroundColor = "#074707";
+            this.style.backgroundColor = "#369b92e8";
         }
 
         signIn.onmouseout = function(){
-        this.style.backgroundColor = "#339c33";
+        this.style.backgroundColor = "##369b92e8";
         }
 
         signUp.onmouseover = function(){
-            this.style.backgroundColor = "#074707";
+            this.style.backgroundColor = "#369b92e8";
         }
 
         signUp.onmouseout = function(){
-        this.style.backgroundColor = "#339c33";
+        this.style.backgroundColor = "#369b92e8";
         }
 
         admin.onmouseover = function(){
-            this.style.backgroundColor = "#074707";
+            this.style.backgroundColor = "#369b92e8";
         }
 
         admin.onmouseout = function(){
-        this.style.backgroundColor = "#339c33";
+        this.style.backgroundColor = "#369b92e8";
         }
 
         submit.onmouseover = function(){
-            this.style.backgroundColor = "#074707";
+            this.style.backgroundColor = "#369b92e8";
         } 
         
         submit.onmouseout = function(){
-            this.style.backgroundColor = "#339c33";
+            this.style.backgroundColor = "#369b92e8";
         }
     
         /* On mouse Click */
         signIn.onclick = function(){
-            this.style.backgroundColor = "#304227";
+            this.style.backgroundColor = "#369b92e8";
             this.style.outline = "none";
         }
 
         signUp.onclick = function(){
-        this.style.backgroundColor = "#304227";
+        this.style.backgroundColor = "#369b92e8";
         this.style.outline = "none";
         }
 
         admin.onclick = function(){
-            this.style.backgroundColor = "#304227";
+            this.style.backgroundColor = "#369b92e8";
             this.style.outline = "none";
             }
 
         submit.onclick = function(){
-        this.style.backgroundColor = "#304227";
+        this.style.backgroundColor = "#369b92e8";
         this.style.outline = "none";
         }
 
         submitButtonAdmin.onclick = function(){
-            this.style.backgroundColor = "#304227";
+            this.style.backgroundColor = "#369b92e8";
             this.style.outline = "none";
         }
 
