27f6c54c6cbeba45f20116c8edab48da80b55540
Joseph <jostheblessing@gmail.com>
2019-03-08 04:23:50 +0200

    [Feature #164443854] Allow user to login

---
+++ server/controllers/login.js
@@ -0,0 +1,28 @@
+import users from '../models/users';
+import Joi from 'joi';
+const bodyParser = require('body-parser');
+
+exports.login = (req, res) =>{
+
+    const {error} = validate(req.body);
+
+    if(error){
+        res.send({status: 400, error: error.details[0].message});
+        return;
+    }
+
+    let user = users.find( email => email.email === req.body.email && email.password === req.body.password);
+    if(!user) return res.send({ status: 400, error: 'User not found'});
+
+    res.send( {status: 200, message: "Logged in successfully"});
+}
+
+const validate =(req) => {
+    const userSchema = {
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required()
+    };
+
+    return Joi.validate(req, userSchema);
+}
+
