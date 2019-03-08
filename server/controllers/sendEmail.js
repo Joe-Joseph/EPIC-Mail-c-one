bec8c79a9184049374c4d3ccb5a1afab75e83eaf
Joseph <jostheblessing@gmail.com>
2019-03-08 05:45:35 +0200

    [Feature #164455830] Allow user to send email to individual

---
+++ server/controllers/sendEmail.js
@@ -0,0 +1,19 @@
+import messages from '../models/messages';
+import moment from 'moment';
+//const uuid = require('uuid');
+
+exports.createEmail = async (req, res) =>{
+    //res.send('Send email');
+
+const message= {
+    id: messages.length + 1,
+    subject: req.body.subject,
+    message: req.body.message,
+    parentId: req.body.parentId,
+    status: "sent",
+    createdOn: moment().format('LL')
+};
+//console.log(message.createdOn);
+await messages.push(message);
+res.send( {status: 200, data: message});
+}
